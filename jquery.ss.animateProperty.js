ee4bfdba6fbc466b89085204eb189c12b272403c
Stuart <stuart@vericode.co.uk>
2015-05-22 17:40:28 +0100

    added fromVal default option
    bit of a bug when changing a colour val and using $(this).css(options.animateWhat), because on successive clicks the animation hasnt finished, so the current colour is somewhere between the original and the toVal...

--- jquery.ss.animateProperty.js
+++ jquery.ss.animateProperty.js
@@ -4,33 +4,38 @@ $.fn.animateProperty = function(options)
     var options = $.extend({
         repeats: 1,
         animateWhat: 'backgroundColor',
-        toVal: '#FFFBCC',
+        fromVal: '#ffffff',
+        toVal: '#fffbcc',
         durationIn: 350,
         durationOut: 1650
     }, options);
 
-    var originalVal = $(this).css(options.animateWhat);
+    var originalVal = options.fromVal || $(this).css(options.animateWhat);
 
     var animFrom = {};
         animFrom[options.animateWhat] = options.toVal;
+
     var animTo = {};
         animTo[options.animateWhat] = originalVal;
 
     var doIt = function(callback)
     {
-        $(this).animate(animFrom, options.durationIn, function()
+        $(this).stop().animate(animFrom, options.durationIn, 'linear', function()
         {
-            $(this).animate(animTo, options.durationOut, callback);
+            $(this).stop().animate(animTo, options.durationOut, 'linear', callback);
         });
+
     };
+
     return this.each(function()
     {
-        var i = +options.repeats;
         var me = this;
+        var i = +options.repeats;
         var loop = function()
         {
-            --i && doIt.call(me, loop)
+            --i && doIt.call(me, loop);
         };
-        doIt.call(this, loop);
+
+        doIt.call(me, loop);
     });
 };
